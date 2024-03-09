41094556b3e255ae9b63de1544baa70031ca4176
chopper <chopper69daddy@gmail.com>
2024-03-09 02:04:06 -0700

    moved clockwork into service

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# Check if the database exists, and if not, create it and migrate
+echo "Database setup"
+bundle exec rake db:create
+bundle exec rake db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
