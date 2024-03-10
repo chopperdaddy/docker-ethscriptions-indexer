735c97225d1c90e5e59775c62e73518e73876145
chopper <chopper69daddy@gmail.com>
2024-03-10 01:55:13 -0700

    consensus controller accepts array

--- app/controllers/consensus_controller.rb
+++ app/controllers/consensus_controller.rb
@@ -1,26 +1,21 @@
 class ConsensusController < ApplicationController
-  # Basic pagination method for simplicity
-  # Adjust according to your application's pagination needs
-  def paginate(query)
-    per_page = params.fetch(:per_page, 10).to_i
-    page = params.fetch(:page, 1).to_i
-    offset = (page - 1) * per_page
-
-    total_count = query.count
-    records = query.limit(per_page).offset(offset)
-
-    [records, { total_count: total_count, page: page, per_page: per_page }]
-  end
-
   def index
-    ethscriptions_scope = Ethscription.select(:transaction_hash, :previous_owner, :current_owner, :creator)
-    
-    # Basic pagination
-    ethscriptions, pagination_info = paginate(ethscriptions_scope)
+    scope = Ethscription.select(:id, :transaction_hash, :current_owner, :creator, :previous_owner)
 
-    render json: { 
-      ethscriptions: ethscriptions.as_json,
-      pagination: pagination_info
-    }
+    transaction_hashes = parse_param_array(params[:transaction_hashes])
+    
+    # Filter by transaction_hash if the parameter is present and is an array
+    if params[:transaction_hashes].present?
+      scope = scope.where(transaction_hash: transaction_hashes)
+    end
+    
+    cache_on_block do
+      results, pagination_response = paginate(scope)
+      
+      render json: {
+        result: numbers_to_strings(results),
+        pagination: pagination_response
+      }
+    end
   end
 end
