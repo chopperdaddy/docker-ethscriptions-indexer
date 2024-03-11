89b6d0ea75d5ea8425bbc3c3503767c3573cd9b2
chopper <chopper69daddy@gmail.com>
2024-03-11 14:01:03 -0600

    fixed race condition with db migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,14 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /apps/tmp/pids/server.pid
 
-# Check if the database exists, and if not, create it and migrate
-echo "Database setup"
-bundle exec rake db:create
-bundle exec rake db:migrate
+# Only run migrations for the specified service
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+  echo "Database setup"
+  bundle exec rake db:create
+  bundle exec rake db:migrate
+else
+  echo "Skipping database setup (migrations)"
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
